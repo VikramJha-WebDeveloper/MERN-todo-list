e87e68564ee264677cf5c08fe9e7067656dafaed
Vikram Jha <jhavikram11121998@gmail.com>
2025-09-12 19:59:10 +0530

    first commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 // import bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,8 +10,8 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
